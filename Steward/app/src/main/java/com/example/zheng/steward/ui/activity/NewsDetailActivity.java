6b6b6e86de9292c5de1cb2c26c25ba40e3534229
Jarvis <zheng.zhao.nj@gmail.com>
2018-06-01 23:24:42 +0800

    消息详情布局

---
+++ Steward/app/src/main/java/com/example/zheng/steward/ui/activity/NewsDetailActivity.java
@@ -0,0 +1,23 @@
+package com.example.zheng.steward.ui.activity;
+
+import com.example.zheng.steward.R;
+import com.example.zheng.steward.ui.base.BaseActivity;
+import com.example.zheng.steward.ui.base.BasePresenter;
+
+public class NewsDetailActivity extends BaseActivity {
+
+    @Override
+    public void initView() {
+        super.initView();
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int provideContentViewId() {
+        return R.layout.activity_news_detail;
+    }
+}
