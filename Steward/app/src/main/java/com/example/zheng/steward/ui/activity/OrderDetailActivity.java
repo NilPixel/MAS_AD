494ccc105a510e2a648a7c3ad2e71af762c2711c
Jarvis <zheng.zhao.nj@gmail.com>
2018-06-03 15:23:55 +0800

    订单详情布局

---
+++ Steward/app/src/main/java/com/example/zheng/steward/ui/activity/OrderDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.zheng.steward.ui.activity;
+
+import com.example.zheng.steward.ui.base.BaseActivity;
+import com.example.zheng.steward.ui.base.BasePresenter;
+
+public class OrderDetailActivity extends BaseActivity{
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int provideContentViewId() {
+        return 0;
+    }
+}
